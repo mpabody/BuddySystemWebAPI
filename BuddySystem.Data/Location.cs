45ee249c6e7f7f81bb5008b4cd7ddc12e962c43c
Michael Pabody <michael.pabody@gmail.com>
2021-03-29 20:46:58 -0400

    added Location table and models

---
+++ BuddySystem.Data/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuddySystem.Data
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        [ForeignKey(nameof(Campus))]
+        public int CampusId { get; set; }
+        public virtual Campus Campus { get; set; }
+
+        [Required]
+        public string LocationName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public string LocationNotes { get; set; }
+    }
+}
---
+++ BuddySystem.Models/LocationModels/LocationCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuddySystem.Models.LocationModels
+{
+    public class LocationCreate
+    {
+        [Required]
+        public int CampusId { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Please enter at least 3 characters.")]
+        [MaxLength(40, ErrorMessage = "There are too many characters in this field. Please enter no more than 40 characters")]
+        [Display(Name = "Location Name")]
+        public string LocationName { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Your address is too short! We need at least 10 characters.")]
+        [MaxLength(100, ErrorMessage = "Your address is too long! It must be less than 100 characters")]
+        public string Address { get; set; }
+
+        [MinLength(3, ErrorMessage = "Please enter at least 3 characters.")]
+        [MaxLength(200, ErrorMessage = "There are too many characters in this field. Please enter no more than 200 characters")]
+        [Display(Name = "Location Notes")]
+        public string LocationNotes { get; set; }
+    }
+}
